5259a4a477dfcb38d6c7e4e39fd68f10bc8b44dd
tkitagawa <tkitagawa0409@nzigen.com>
2023-05-23 10:32:02 +0900

    php-cs-fixerの導入2

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,14 +1,25 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__)
-    ->exclude('vendor');
+    ->in([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
 
-return PhpCsFixer\Config::create()
-    ->setRiskyAllowed(true)
+$config = new PhpCsFixer\Config();
+
+return $config
     ->setRules([
         '@PSR2' => true,
         'array_syntax' => ['syntax' => 'short'],
         'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'no_unused_imports' => true,
     ])
     ->setFinder($finder);
